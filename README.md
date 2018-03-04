b4ee58e50bbd07ff68021de98d57f431a8aef90e
Canna Wen <cannawen@gmail.com>
2018-03-04 22:42:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# flights
+
+App that queries airport and route information
+
+## To run
+
+`lein repl`
+`(import!)`
+`(run)`
+
+To reload after changes
+`(require 'flights.core :reload)`
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject flights "0.0.1"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [datascript "0.16.3"]]
+  :main flights.core)
